c072bc6d4ab5c42813bf851716ee2147e2ad3f42
Jaskaran Singh <42887065+jaskaran-5ingh@users.noreply.github.com>
2024-04-20 03:19:15 +0000

    added script for google analtics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,59 +25,72 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <meta
-        name="keywords"
-        content="Developer, React, React Native, Backend, SEO, Amritsar, Punjab, India, Software Development, Frontend, Full Stack, Mobile App Development, Web Development, Programming, IT Services, Digital Marketing, Tech Solutions, Coding, JavaScript, Node.js, MongoDB, Express.js, UI/UX, Search Engine Optimization, App Design, Software Engineer, IT Company, Technology, Innovation"
-      />
-      <meta
-        name="description"
-        content="I love writing code that takes things next level creating highly performant websites, automated API integrations, building my own dev-tools, and creating stunning user-experiences that makes you feel WOW!."
-      />
-      <meta name="author" content="Jaskaran Singh" />
-      <meta name="robots" content="index, follow" />
-      <meta property="og:title" content="Jaskaran Singh" />
-      <meta
-        property="og:description"
-        content="I love writing code that takes things next level creating highly performant websites, automated API integrations, building my own dev-tools, and creating stunning user-experiences that makes you feel WOW!."
-      />
-      <meta
-        property="og:image"
-        content="https://avatars.githubusercontent.com/u/42887065?v=4"
-      />
-      <meta property="og:url" content="https://jaskaransingh.info" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Jaskaran Singh" />
-      <meta
-        name="twitter:description"
-        content="I love writing code that takes things next level creating highly performant websites, automated API integrations, building my own dev-tools, and creating stunning user-experiences that makes you feel WOW!."
-      />
-      <meta
-        name="twitter:image"
-        content="https://avatars.githubusercontent.com/u/42887065?v=4"
-      />
-      <meta name="twitter:url" content="https://jaskaransingh.info" />
+      <head>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="keywords"
+          content="Developer, React, React Native, Backend, SEO, Amritsar, Punjab, India, Software Development, Frontend, Full Stack, Mobile App Development, Web Development, Programming, IT Services, Digital Marketing, Tech Solutions, Coding, JavaScript, Node.js, MongoDB, Express.js, UI/UX, Search Engine Optimization, App Design, Software Engineer, IT Company, Technology, Innovation"
+        />
+        <meta
+          name="description"
+          content="I love writing code that takes things next level creating highly performant websites, automated API integrations, building my own dev-tools, and creating stunning user-experiences that makes you feel WOW!."
+        />
+        <meta name="author" content="Jaskaran Singh" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Jaskaran Singh" />
+        <meta
+          property="og:description"
+          content="I love writing code that takes things next level creating highly performant websites, automated API integrations, building my own dev-tools, and creating stunning user-experiences that makes you feel WOW!."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/42887065?v=4"
+        />
+        <meta property="og:url" content="https://jaskaransingh.info" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Jaskaran Singh" />
+        <meta
+          name="twitter:description"
+          content="I love writing code that takes things next level creating highly performant websites, automated API integrations, building my own dev-tools, and creating stunning user-experiences that makes you feel WOW!."
+        />
+        <meta
+          name="twitter:image"
+          content="https://avatars.githubusercontent.com/u/42887065?v=4"
+        />
+        <meta name="twitter:url" content="https://jaskaransingh.info" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KGEBTE2XRW"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-KGEBTE2XRW');
+          `}
+        </script>
+      </head>
       <body className={inter.className}>
         <Providers>
-        {/* <StartBackground /> */}
+          {/* <StartBackground /> */}
           <Image
             alt="background"
             src="/bg-grid-lighter.4c1e8196.svg" // Replace with your image path
@@ -88,19 +101,10 @@ export default function RootLayout({
           />
           <Header />
           {children}
-          <Footer/>
-          <SpeedInsights/>
-          <Analytics/>
+          <Footer />
+          <SpeedInsights />
+          <Analytics />
         </Providers>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KGEBTE2XRW"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-KGEBTE2XRW');
-        </script>
       </body>
     </html>
   );
