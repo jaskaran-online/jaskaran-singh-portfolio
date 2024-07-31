08663a8d06da14f9c523fd65cf6fc8c2bccdcda5
Jaskaran Singh <jaskaransingh4704@gmail.com>
2024-07-31 12:00:01 +0530

    Remove profile pic from dashboard

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -12,9 +12,9 @@ const AboutMeSection = () => {
         <Tag label="About me" />
       </div>
 
-      <div className="flex w-full flex-col justify-between gap-12 md:flex-row">
+      <div className="flex w-full flex-col justify-center gap-12 md:flex-row">
         {/* Image */}
-        <div className="flex justify-center md:order-first md:justify-end">
+        {/* <div className="flex justify-center md:order-first md:justify-end">
           <div className="relative h-[380px] w-[320px] md:h-[460px] md:w-[380px] lg:h-[520px] lg:w-[440px]">
             <Image
               src={jaskaransingh}
@@ -24,11 +24,11 @@ const AboutMeSection = () => {
             ></Image>
             <div className="absolute h-[360px] w-[320px] border-8 border-transparent bg-gray-200 max-md:top-5 md:bottom-0 md:left-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"></div>
           </div>
-        </div>
+        </div> */}
 
         {/* Content */}
-        <div className="flex max-w-xl flex-col gap-6">
-          <Typography variant="h3">
+        <div className="flex flex-col gap-6 justify-center items-center">
+          <Typography variant="h3" className="text-center">
             Curious about me? Here you have it:
           </Typography>
           <Typography>
